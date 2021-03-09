b50bd1768f7b45a399a22ca361ae863373e49b2e
alberty4573 <shenlieku@gmail.com>
2021-03-09 14:41:56 +1100

    Initial check-in of module WordReversal

---
+++ WordReversal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Requirement: For any given sentence, reverse the characters of each word in the sentence.
+
+            // Please change the input 
+            string input = "This is Albrt.";
+            var newReverse = new WordReverse();
+            var outPut = newReverse.Reverse(input);
+            Console.WriteLine(outPut);
+        }
+    }
+}
---
+++ WordReversal/WordReverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WordReversal
+{
+    class WordReverse
+    {
+        public string Reverse(string input)
+        {
+            string result = "";
+
+            // seperate each word based on space
+            string[] words = input.Split(' ');
+            foreach (string word in words)
+            {
+                char[] charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+                string newWord = new string(charArray);
+                result = result + newWord + " ";
+            }
+
+
+            return result;
+        }
+    }
+}
